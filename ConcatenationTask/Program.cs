13e1aa2092315b9f017cf1ecef2ff0e99372c843
Reva Tandale <22RTandale@heckgrammar.co.uk>
2025-01-26 20:09:41 +0000

    finsihed

--- ConcatenationTask/Program.cs
+++ ConcatenationTask/Program.cs
@@ -5,14 +5,21 @@
         static void Main(string[] args)
         {
             string FirstName;
+            string LastName;
+            int age;
             Console.Write("Enter first name: ");
             FirstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Enter age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
             // add two more variables for the users last name and their age
             // take these inputs into the program
             // use the test data Bob, Smith, 14
             // ammend this code to output in one command
             // Hello Bob Smith you are 14 years old
-            Console.WriteLine(FirstName);
+            Console.WriteLine("Hello " + FirstName + " " + LastName + "you are " + age + " years old");
             // save and commit your program
             // add evidence of the test input and your own input running the program
         }
